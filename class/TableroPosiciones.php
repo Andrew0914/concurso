b717b3144252d2075116b946bcf65dae7fd3e31c
AndrewAlan <andrewalangm@gmail.com>
2019-05-12 15:10:00 -0500

    TableroPosiciones class refactoring

--- class/TableroPosiciones.php
+++ class/TableroPosiciones.php
@@ -6,13 +6,17 @@
 	require_once dirname(__FILE__) . '/TableroPaso.php';
 	require_once dirname(__FILE__) . '/Concurso.php';
 	require_once dirname(__FILE__). '/Concursante.php';
+	require_once dirname(__FILE__). '/Concursante.php';
+	require_once dirname(__FILE__).'/util/Response.php';
 	
 	class TableroPosiciones extends BaseTable{
 
 		protected $table='tablero_posiciones';
+		private $response;
 
 		public function __construct(){
 			parent::__construct();
+			$this->response = new Response();
 		}
 
 		/**
@@ -47,20 +51,12 @@
 		    return ($a['totalPuntos'] > $b['totalPuntos']) ? -1 : 1;
 		}
 
-		/**
-		 * Genera y almacena las posiciones de los puntajes
-		 * @param  array $mejores  
-		 * @param  integer $concurso 
-		 * @return boolean           
-		 */
-		public function generaPosiciones($concurso,$es_empate = false){
-			$valida = 1;
-			$master = new TableroMaster();
-			$id_master = $master->guardar(['ID_CONCURSO' => $concurso]);
+		private function getMejoresOnTableroMaster($concurso,$es_empate){
 			$puntaje = new TableroPuntaje();
 			$mejores = $puntaje->getMejoresPuntajes($concurso,$es_empate)['mejores'];
 			$objConcurso = new Concurso();
 			$objConcurso = $objConcurso->getConcurso($concurso);
+
 			// si es grupal contamos las puntuacioens del paso para medir las posiciones
 			if($objConcurso['ID_ETAPA'] == 2){
 				$pasos = new TableroPaso();
@@ -79,10 +75,23 @@
 					$mejores[$i]['lugar'] = $i+1;
 				}
 			}
-			if($id_master <= 0){
-				return ['estado'=>0 , 'mensaje' => 'No se pudo generar el tablero maestro'];
-			}
-			
+			return $mejores;
+		}
+
+		/**
+		 * Genera y almacena las posiciones de los puntajes
+		 * @param  array $mejores  
+		 * @param  integer $concurso 
+		 * @return boolean           
+		 */
+		public function generaPosiciones($concurso,$es_empate = false){
+			$master = new TableroMaster();
+			$id_master = $master->guardar(['ID_CONCURSO' => $concurso]);
+
+			if($id_master <= 0) return $this->response->fail('No se pudo generar el tablero maestro');
+
+			$mejores = $this->getMejoresOnTableroMaster($concurso, $es_empate);
+
 			// almacenamos las posiciones brutas
 			foreach ($mejores as $mejor) {
 				$posicion = [ 'ID_CONCURSANTE' => $mejor['ID_CONCURSANTE']
@@ -90,56 +99,41 @@
 							,'PUNTAJE_TOTAL'=>$mejor['totalPuntos']
 							,'ID_TABLERO_MASTER' => $id_master
 							,'EMPATADO' => 0 ];
-				if(!$this->guardar($posicion)){
-					$valida *= 0;
-				}
+
+				if(!$this->guardar($posicion)) return $this->fail('No se alamacenaron las posiciones correctamente');
 			}
 
-			if(!$valida){
-				return ['estado'=>0 , 'mensaje'=> 'No se alamacenaron las posiciones correctamente'];
-			}
 			// calculamos los empates
-			$valida = 1;
 			$posiciones = $this->obtenerPosicionesActuales($id_master);
 			foreach ($posiciones as $p) {
 				foreach ($posiciones as $ps) {
 					if($p['ID_CONCURSANTE'] != $ps['ID_CONCURSANTE'] 
 						AND $p['PUNTAJE_TOTAL'] == $ps['PUNTAJE_TOTAL']
 						AND $p['POSICION'] <= 3){
-						if(!($this->update($ps['ID_TABLERO_POSICION'],['EMPATADO'=>1])
-							AND $this->update($p['ID_TABLERO_POSICION'] , ['EMPATADO'=>1]))){
-							$valida *= 0;
-						}
+						if(!($this->update($ps['ID_TABLERO_POSICION'],['EMPATADO'=>1]) 
+						AND $this->update($p['ID_TABLERO_POSICION'] , ['EMPATADO'=>1]))) return $this->response->fail('No se calcularon los empates');
+
 						// cambiamos posicion (solo vivusal para las medallas)
 						if(!$this->cambioMiposicion($ps['ID_CONCURSANTE'],$ps['ID_TABLERO_MASTER'])){
 							$this->update($ps['ID_TABLERO_POSICION'] , ['POSICION'=>$p['POSICION'] , 'POSICION_CAMBIO'=>1]);
 							$this->update($p['ID_TABLERO_POSICION'] , ['POSICION_CAMBIO'=>1]);
 						}
+
 					}
 				}
 			}
 
-			if(!$valida){
-				return ['estado'=>0 , 'mensaje'=> 'No se calcularon los empates'];
-			}
+			if( !$master->actualiza($id_master ,['POSICIONES_GENERADAS' => 1]) ) return $this->response->fail('No se pudo establecer la bandera de posiciones generadas');
 
-			// por utlimo indicamos que ya las posiciones fueron genradas por temas del timer
-			$master = new TableroMaster();
-			
-			if( !$master->actualiza($id_master ,['POSICIONES_GENERADAS' => 1]) ){
-				return ['estado'=>'No se pudo establecer la bandera de posiciones generadas'];
-			}
-
-			return ['estado'=>1 , 'mensaje' => 'Tableros generados' , 'tablero_master'=>$id_master ];
+			return $this->response->success(['tablero_master'=>$id_master] , 'Tableros generados');
 		}
-
-		
 
 		public function cambioMiposicion($concursante, $master){
 			$where = "ID_CONCURSANTE = ? AND ID_TABLERO_MASTER = ?";
 			$valores = ['ID_CONCURSANTE'=>$concursante , 'ID_TABLERO_MASTER' => $master];
 			return $this->get($where, $valores)[0]['POSICION_CAMBIO'];
 		}
+
 		/**
 		 * Obtiene los tableros actuales del concurso
 		 * @param  integer $concurso      
@@ -148,47 +142,38 @@
 		 * @return array                
 		 */
 		public function getTableros($concurso,$master_creado,$es_empate){
-			$response = ['estado'=> 0 , 'mensaje' => 'No se pudieron obtener los tableros']; 
 			$puntajes = new TableroPuntaje();
 			$master = new TableroMaster();
 			$pasos = new TableroPaso();
-			$mejores = $puntajes->getMejoresPuntajes($concurso,$es_empate)['mejores'];
 			if($master_creado > 0){
-				$response['master']= $master->getMaster($master_creado);
 				$sentencia = "SELECT tp.*,c.CONCURSANTE FROM tablero_posiciones tp INNER JOIN concursantes c ON tp.ID_CONCURSANTE = c.ID_CONCURSANTE  WHERE tp.ID_TABLERO_MASTER = ? GROUP BY c.ID_CONCURSANTE ORDER BY tp.POSICION";
-				$response['posiciones'] = $this->query($sentencia,['ID_TABLERO_MASTER'=>$master_creado]);
-				$response['puntajes'] = $puntajes->getResultados($concurso,$es_empate,false);
-				$response['pasos'] = $pasos->getResultados($concurso);
-				$response['estado'] = 1;
-				$response['mensaje'] = 'Se obtuvieron los tableros exitosamente';
+				return $this->response->success([
+						'master' => $master->getMaster($master_creado),
+						'posiciones' => $this->query($sentencia,['ID_TABLERO_MASTER'=>$master_creado]),
+						'puntajes' => $puntajes->getResultados($concurso,$es_empate,false),
+						'pasos' => $pasos->getResultados($concurso)
+					],'Se obtuvieron los tableros exitosamente');
 			}
-
-			return $response;
+			return $this->response->fail('No se pudieron obtener los tableros');
 		}
 
 		public function esEmpate($master){
-			$where = "ID_TABLERO_MASTER = ?";
-			$valores = ['ID_TABLERO_MASTER' => $master];
-			$response = ['estado'=>0, 'mensaje'=> 'No se determino el empate correctamente'];
-			$rs = $this->get($where,$valores);
+			$posiciones = $this->get("ID_TABLERO_MASTER = ?",['ID_TABLERO_MASTER' => $master]);
 			$emptatados = 0;
 			$concursante = new Concursante();
-			for($i = 0; $i < count($rs) ; $i++) {
-				if($rs[$i]['EMPATADO'] == 1){
+			for($i = 0; $i < count($posiciones) ; $i++) {
+				if($posiciones[$i]['EMPATADO'] == 1){
 					$emptatados++;
 				}
-				$rs[$i]['CONCURSANTE'] = $concursante->getConcursante($rs[$i]['ID_CONCURSANTE'])['CONCURSANTE'];
+				$posiciones[$i]['CONCURSANTE'] = $concursante->getConcursante($posiciones[$i]['ID_CONCURSANTE'])['CONCURSANTE'];
 			}
 
 			if($emptatados > 0){
-				$response['estado'] = 1;
-				$response['mensaje'] ="Se genero empate";
+				return $this->response->success(['empatados' => $posiciones ] , 'Se genero empate');
 			}else{
-				$response['estado'] = 2;
-				$response['mensaje'] = 'No se genero ningun empate';
+				return ['estado' => 2 , 'mensaje'=>'No se genero empate' , 'empatados' => $posiciones];
 			}
-			$response['empatados'] = $rs;
-			return $response;
+			return $this->response->fail('No se genero ningun empate');
 		}
 
 		public function esEmpateByConcurso($idConcurso){
