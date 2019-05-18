9fc5c00adff58105f3e7d015d6a49f9af021ebbb
AndrewAlan <andrewalangm@gmail.com>
2019-05-18 14:18:26 -0500

    Add readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-"# concurso" 
+# Concurso de preguntas
+Hecho con:
+* PHP
+* MYSQL
+* JS
+* HTML Y CSS+
+
+Se utiliza una forma de programacion o estilo tipo comenta para simular tiempo real entre un script de php y un script de js.
+
--- test.php
+++ test.php
@@ -1,12 +1,3 @@
 <?php
-   require_once dirname(__FILE__) . '/class/database/BaseTable.php';
-
-   function generaQuerys($pregunta , $nuevaPregunta , $respuestaA , $respuestaB, $respuestaC,$respuestaD, $correcta){
-      $select = "SELECT ID_PREGUNTA WHERE PREGUNTA = '%". $pregunta ."%'";
-      $queryPregunta = "UPDATE preguntas SET PREGUNTA = '". $nuevaPregunta . "' WHERE PREGUNTA LIKE '%".$pregunta."%'";
-      $queryResetRespuesta = "UPDATE respuestas SET ES_CORRECTA = 0 WHERE ID_PREGUNTA = (".$select.")";
-      echo $queryPregunta
-   }
-
-
+   echo "TESTING";
 ?>
